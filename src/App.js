4f8830eb9430594f305137e8322ea6cd5f633be5
SaeidiAmirMohammad <amirmohammad1379.saeidi@gmail.com>
2021-11-07 23:42:53 +0330

    State & SetState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 import Card from './Card';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App App-link">
-    <Card headTitle="headTitle 1" body="bodyCard_1-bodyCard_1-bodyCard_1-bodyCard_1-bodyCard_1-bodyCard_1-bodyCard_1"/>
-    <Card headTitle="headTitle 2" body="bodyCard_2-bodyCard_2-bodyCard_2-bodyCard_2-bodyCard_2-bodyCard_2-bodyCard_2"/>
-    <Card footerText="footerText" body="footer-footer-footer-footer-footer-footer-footer"/>
+// function App() {
+//   return (
+//     <div className="App App-link">
+//     <Card headTitle="headTitle 1" body="bodyCard_1-bodyCard_1-bodyCard_1-bodyCard_1-bodyCard_1-bodyCard_1-bodyCard_1"/>
+//     <Card headTitle="headTitle 2" body="bodyCard_2-bodyCard_2-bodyCard_2-bodyCard_2-bodyCard_2-bodyCard_2-bodyCard_2"/>
+//     <Card footerText="footerText" body="footer-footer-footer-footer-footer-footer-footer"/>
 
-    </div>
-  );
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  state = {
+    articles: [
+      { id: 1, title: "oneTitle", text: "welcome To ReactApp_1" , footer: "footerText_1"},
+      { id: 2, title: "twoTitle", text: "welcome To ReactApp_2" , footer: "footerText_2"},
+      { id: 3, title: "threeTitle", text: "welcome To ReactApp_3" , footer: "footerText_3"}
+
+    ]
+  }
+
+  constructor(){
+    super()
+    setTimeout(() => {
+      this.setState ({
+        articles: [
+          { id: 4, title: "oneTitle", text: "welcome To ReactApp_4" , footer: "footerText_4"},
+          { id: 5, title: "twoTitle", text: "welcome To ReactApp_5" , footer: "footerText_5"},
+          { id: 6, title: "threeTitle", text: "welcome To ReactApp_6" , footer: "footerText_6"}
+    
+        ]
+      })
+    }, 2000);
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Card headTitle={this.state.articles[0].title} body={this.state.articles[0].text} footerText={this.state.articles[0].footer} />
+        <Card headTitle={this.state.articles[1].title} body={this.state.articles[1].text} footerText={this.state.articles[1].footer}/>
+        <Card headTitle={this.state.articles[2].title} body={this.state.articles[2].text} footerText={this.state.articles[2].footer}/>
+      </div>
+    )
+  }
 }
 
 export default App;
